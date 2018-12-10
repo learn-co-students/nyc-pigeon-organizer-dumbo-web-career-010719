e65b025a35d69329eca12288d92c0e0950e1f46a
mazenswar <mazenswar@protonmail.com>
2018-12-09 20:34:58 -0500

    nyc pigeons

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,32 @@
+require "pry"
+
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  birds = {}
+
+  data[:gender].each do |k, v|
+    v.each do |k1,v1|
+      birds[k1] = {:gender => [k.to_s]}
+    end
+  end
+
+  data[:color].each do |k, v|
+    v.each do |k1,v1|
+      if birds[k1][:color].nil?
+        birds[k1].merge!({:color => [k.to_s]})
+      else
+        birds[k1][:color] << k.to_s
+      end
+    end
+  end
+
+  data[:lives].each do |k, v|
+    v.each do |k1,v1|
+      birds[k1].merge!({:lives => [k.to_s]})
+    end
+  end
+
+  birds
+
 end
