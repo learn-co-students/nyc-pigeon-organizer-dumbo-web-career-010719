aea4fbe7fc114c81a692ddb50a5c7774c4af468c
Edwin Lin <edwinlin@gmail.com>
2018-12-13 22:41:39 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  
+  data.each do |type, hash|
+    hash.each do |specific, names|
+      names.each do |name|
+        new_hash[name] ||= {}
+        new_hash[name][type] ||= []
+        new_hash[name][type] << specific.to_s
+
+      end
+    end
+  end
+
+  return new_hash
 end
+
