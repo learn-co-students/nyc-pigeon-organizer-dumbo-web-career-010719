6b96b0cc6ed26d4abfe285ae5ddd832678c6b258
Weijun Huang <wjhuang0726@gmail.com>
2018-12-19 20:17:44 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,42 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  hash = Hash.new
+  data.each do |k, v|
+    v.each do |t, s|
+      s.each do |i|
+      hash[i]={:color =>[], :gender =>[], :lives =>[] }
+    end
+    end
+  end
+
+  hash.each do |k, v|
+    data[:color].each do |t, s|
+      s.each do |i|
+        if i.to_s == k.to_s
+          v[:color]<<t.to_s
+        end
+      end
+    end
+  end
+
+   hash.each do |k, v|
+    data[:gender].each do |t, s|
+      s.each do |i|
+        if i.to_s == k.to_s
+          v[:gender]<<t.to_s
+        end
+      end
+    end
+  end
+
+   hash.each do |k, v|
+    data[:lives].each do |t, s|
+      s.each do |i|
+        if i.to_s == k.to_s
+          v[:lives]<<t.to_s
+        end
+      end
+    end
+  end
+  hash
 end
