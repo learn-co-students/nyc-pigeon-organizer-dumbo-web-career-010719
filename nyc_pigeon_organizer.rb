53e8ec9321fb1fb377fdd27504f14fb0a8b1b57a
github username <github email address>
2019-01-02 15:53:14 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+require 'pry'
+def nyc_pigeon_organizer(data)	# takes argument of hash (pigeon_data), iterates over hash, and builds new hash with pigeon names as keys pointing to value of hash describing attributes/values
+	pigeon_list = {}			# set a variable equal to an empty hash, adding new key/value pairs throughout iteration
+
+	# names = data[:color].each {|keys, values| values}.values.flatten	# creates array of unique names
+	
+	# cgl = data.keys			# creates array of data hash keys
+	# cgl_hash = {}			# creates empty hash for color/gender/lives
+	# cgl.each { |attribute| cgl_hash[attribute] = [] }		# iterates through cgl array, adding :color/:gender/:lives keys equal to empty arrays
+
+	# names.each { |name| pigeon_list[name] = cgl_hash }		# iterates through names array, adding each name as a key, each with a cgl_hash
+	
+	data.each do |k, v|
+		v.each do |k1, v1|
+			if pigeon_list[v1] == nil
+				pigeon_list[v1] = {}
+			end
+			pigeon_list[v1][:color] ||= []
+			pigeon_list[v1][:color] << data[:color].key(v1)
+			pigeon_list[v1][:gender] ||= []
+			pigeon_list[v1][:gender] << data[:gender].key.v1
+			pigeon_list[v1][:lives] ||= []
+			pigeon_list[v1][:lives] << data[:live].key.v1
+		end	
+	end
+	pigeon_list
+	binding.pry
 end
